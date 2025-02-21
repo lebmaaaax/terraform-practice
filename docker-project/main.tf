c16cbf503c0dbbf41892bcb8371fdd31d71e8695
lebmaaax <admin@compute-vm-2-2-10-ssd-1739533335203.ru-central1.internal>
2025-02-21 12:22:41 +0000

    postrgres + network added

--- docker-project/main.tf
+++ docker-project/main.tf
@@ -11,16 +11,54 @@ provider "docker" {
    host = "unix:///var/run/docker.sock"
 }
 
+resource "docker_network" "app_network" {
+   name = "app_network"
+}
+
+resource "docker_volume" "app_network" {
+   name = "app_network"
+}
+
 resource "docker_image" "nginx" {
    name = "nginx:latest"
    keep_locally = true
 }
 
 resource "docker_container" "nginx" {
-  image = "nginx:latest"
-  name  = "nginx_terraform"
+   image = docker_image.nginx.name
+   name  = "nginx_server"
+   network_mode = docker_network.app_network.name
+
+   ports {
+     internal = 80
+     external = 8080
+   }
+}
+
+resource "docker_volume" "pg_data" {
+   name = "pg_data"
+}
+
+resource "docker_container" "postgres" {
+   image = "postgres:15"
+   name = "postgres_db"
+
+   env = [
+      "POSTGRES_USER=admin",
+      "POSTGRES_PASSWORD=admin",
+      "POSGRESS_DB=mydb"
+   ]
+
+  network_mode = docker_network.app_network.name
+
+  volumes {
+    volume_name = docker_volume.pg_data.name
+    container_path = "/var/lib/postgresql/data" 
+  }
+  
   ports {
-    internal = 80
-    external = 8080
+     internal = 5432
+     external = 5432
   }
 }
+ 
